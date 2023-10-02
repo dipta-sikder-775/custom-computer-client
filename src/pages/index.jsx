42549a9731800a835634d12974b53c3fca99af91
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-03 01:36:00 +0600

    - home page design fix

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,7 @@
-import FeaturedCategory from '@/components/FeaturedCategory'
-import FeaturedProduct from '@/components/FeaturedProduct'
-import RootLayout from '@/components/RootLayout'
-import Head from 'next/head'
+import FeaturedCategory from "@/components/FeaturedCategory";
+import FeaturedProduct from "@/components/FeaturedProduct";
+import RootLayout from "@/components/RootLayout";
+import Head from "next/head";
 
 export default function Home({ data, category }) {
   return (
@@ -9,29 +9,39 @@ export default function Home({ data, category }) {
       <Head>
         <title>Build PC</title>
       </Head>
-      <img className='w-full  md:h-[500px]' src="https://i.ibb.co/BsdJWGY/Capture.png" alt="" />
+
+      <img
+        className="w-full  md:h-[500px]"
+        src="https://i.ibb.co/BsdJWGY/Capture.png"
+        alt=""
+      />
+
       <FeaturedProduct data={data} />
+
       <FeaturedCategory data={category} />
     </div>
-  )
+  );
 }
 
 Home.getLayout = function getLayout(page) {
-  return <RootLayout>{page}</RootLayout>
-}
+  return <RootLayout>{page}</RootLayout>;
+};
 
 export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://building-pc.vercel.app/api/v1/product/random-products"
+  );
+  const data = await res.json();
 
-  const res = await fetch('https://building-pc.vercel.app/api/v1/product/random-products')
-  const data = await res.json()
-  const response = await fetch('https://building-pc.vercel.app/api/v1/category/categorys')
-  const category = await response.json()
+  const response = await fetch(
+    "https://building-pc.vercel.app/api/v1/category/categorys"
+  );
+  const category = await response.json();
+  
   return {
     props: {
       data,
       category,
     },
   };
-
-}
-
+};
