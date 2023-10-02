fb0dea50a5c08db3aff6ca8d4c25225044176423
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-03 05:56:09 +0600

    - not found page designed

---
+++ src/pages/404.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div className='h-[100vh] flex justify-center items-center'>
+            <h1 className='text-5xl font-semibold'><span className='text-7xl'>404</span> Error Page</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
