d3d290be16773755e804f014a8a587665b2bba8d
dipta-sikder-775 <anotheramit8@gmail.com>
2023-10-03 07:20:15 +0600

    - product api integrated

---
+++ src/redux/product/productApi.js
@@ -0,0 +1,18 @@
+const { apiSlice } = require("../api/apiSlice");
+
+const productApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: (id) => `/product/category-products/${id}`
+        }),
+        getRandomProducts: builder.query({
+            query: () => `/product/random-products`
+        }),
+        getSingleProduct: builder.query({
+            query: (id) => `/product/single-product/${id}`
+        }),
+    })
+})
+
+
+export const {useGetProductsQuery, useGetSingleProductQuery, useGetRandomProductsQuery} = productApi;
